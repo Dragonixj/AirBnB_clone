3baa4f72aeb0f822216fc8f39d7b6501f3be3945
Dragonixj <delafiado@gmail.com>
2023-10-11 14:35:38 +0000

    added some test cases

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,2 +1,42 @@
 #!/usr/bin/python3
 """Defines unittests for models/base_model.py"""
+import unittest
+import os
+from datetime import datetime
+import models
+from time import sleep
+from models.base_model import BaseModel
+
+
+class TestBaseModel(unittest.TestCase):
+    """
+    Test suite for the models.base_model.BaseModel
+    """
+
+    def test_str_representation(self):
+        """
+        Checks if the string representation is correct
+        """
+        bm = BaseModel()
+        self.assertEqual(
+            str(bm), "[BaseModel] ({}) {}".format(bm.id, bm.__dict__))
+
+    def test_if_BaseModel_instance_has_id(self):
+        """Checks if id is assigned to instance during initialization"""
+        bm = BaseModel()
+        self.assertTrue(hasattr(bm, "id"))
+
+    def test_if_id_is_unique(self):
+        """
+        Checks if id is generated uniquely
+        """
+        bm1 = BaseModel()
+        bm2 = BaseModel()
+        self.assertNotEqual(bm1.id, bm2.id)
+
+    def test_id_type_is_str(self):
+        """
+        Checks that id is a str object
+        """
+        bm = BaseModel()
+        self.assertTrue(str, type(bm.id))
