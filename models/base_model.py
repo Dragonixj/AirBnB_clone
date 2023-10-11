8c4fc401e412dbd66420fa0ac58408e5f8ef5791
Dragonixj <delafiado@gmail.com>
2023-10-11 13:51:24 +0000

    added the instantiation of base model and created the test file

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,7 @@
 """
 A module that implements the BaseModel class
 """
-from uuid import uuid4
+import uuid
 from datetime import datetime
 
 
@@ -12,4 +12,31 @@ class BaseModel:
     """
 
     def __init__(self, *args, **kwargs):
-        """Initializes the instance attributes"""
+        """Initializes the instance attributes
+
+        Args:
+            *args: list of arguments
+            **kwargs: dict of key-values arguments
+        """
+        if kwargs is not None and kwargs != {}:
+            for key in kwargs:
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f"
+                    )
+                elif key == "upadated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f"
+                    )
+                else:
+                    self.__dict__[key] = kwargs[key]
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+    def __str__(self):
+        """Return the string representation
+        [<class name>] (<self.id>) <self.__dict__>
+        """
+        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
