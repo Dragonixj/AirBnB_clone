5e77ad5ffc070bd5ce25f7653436ac29363c2255
Dragonixj <delafiado@gmail.com>
2023-10-12 12:40:45 +0000

    Task3: added the Public instance method to_dect

--- models/base_model.py
+++ models/base_model.py
@@ -15,8 +15,8 @@ class BaseModel:
         """Initializes the instance attributes
 
         Args:
-            *args: list of arguments
-            **kwargs: dict of key-values arguments
+            *args: list of arguments(Unused)
+            **kwargs: dict that contains attribute values
         """
         if kwargs is not None and kwargs != {}:
             for key in kwargs:
@@ -31,6 +31,7 @@ class BaseModel:
                 else:
                     self.__dict__[key] = kwargs[key]
         else:
+            # Initializes a new instance
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
@@ -50,4 +51,8 @@ class BaseModel:
         returns a dictionary containing all key/values of __dict__
         of instance
         """
-        pass
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = type(self).__name__
+        my_dict["created_at"] = my_dict["created_at"].isoformat()
+        my_dict["updated_at"] = my_dict["updated_at"].isoformat()
+        return my_dict
