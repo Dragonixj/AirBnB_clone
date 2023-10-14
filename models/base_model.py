5a6afe8ddd12f05178936136005f3bd0b7f1d325
Dragonixj <delafiado@gmail.com>
2023-10-14 11:09:43 +0000

    formatted some code for more consistency and clarity

--- models/base_model.py
+++ models/base_model.py
@@ -22,10 +22,9 @@ class BaseModel:
             # Initializes attributes from kwargs
             for key, value in kwargs.items():
                 if key != "__class__":
-                    if key == "created_at" or key == "updated_at":
+                    if key in ("created_at", "updated_at"):
                         setattr(
-                            self, key, datetime.strptime(
-                                value, "%Y-%m-%dT%H:%M:%S.%f")
+                            self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                         )
                     else:
                         setattr(self, key, value)
