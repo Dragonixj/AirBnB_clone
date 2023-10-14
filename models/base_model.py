34029abcb2b5656f9878a37b07aa3ee98086ac94
Dragonixj <delafiado@gmail.com>
2023-10-14 11:23:15 +0000

    added the instance storage to link FileStorage to BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@ A module that implements the BaseModel class
 """
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
@@ -34,6 +35,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """Return the string representation
@@ -43,7 +45,9 @@ class BaseModel:
 
     def save(self):
         """Updates self.updated_at with the current datetime"""
+
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """
